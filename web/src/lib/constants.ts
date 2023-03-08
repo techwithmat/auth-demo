103b826a69d66839c4e59a542f700f2485028326
Mateo2131 <goz.matu@gmail.com>
2023-03-08 17:22:26 -0300

    fix: reading env variables with vite

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -1,3 +1,3 @@
-export const DEVELOPMENT_URL = import.meta.env.DEV
-  ? import.meta.env.DEVELOPMENT_URL
-  : import.meta.env.PRODUCTION_URL
+export const DEVELOPMENT_URL: string = import.meta.env.DEV
+  ? import.meta.env.VITE_DEVELOPMENT_URL
+  : import.meta.env.VITE_PRODUCTION_URL
